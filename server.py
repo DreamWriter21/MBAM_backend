da6fb9b68ccba095a66abc6d48b5d70a45849b0f
DreamWriter21 <39731022+DreamWriter21@users.noreply.github.com>
2025-10-16 01:28:49 +0200

    add main action to server.py

--- server.py
+++ server.py
@@ -22,5 +22,6 @@ def upload_chunk():
 
     return "OK\n"
 
-# ⚠️ Ne rien exécuter automatiquement ici
-# Railway lancera le serveur depuis entrypoint.py
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 8080))
+    app.run(host="0.0.0.0", port=port)
